dd90936db7036f2ab1fec58e2a812c8a07fa102b
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-06 18:08:14 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,12 +1,12 @@
 def square_array(array)
-   array = []
+   sq_array = []
    array.each do |number|
    number = (number ** 2)
-   array << number
+   sq_array << number
    end
-   array
+   sq_array
 end
 
 def new_square_array(array)
-  array.collect{|number| number * 2}
+  sq_array.collect{|number| number * 2}
 end
